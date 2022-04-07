014a1403028b8cc4c734c9cf416f625bd5fe769a
kotche <kot313@gmail.com>
2022-04-07 12:18:28 +0300

    Move commands to separate package

---
+++ internal/app/commands/commander.go
@@ -0,0 +1,17 @@
+package commands
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/kotche/bot/internal/service/product"
+)
+
+type Commander struct {
+	bot            *tgbotapi.BotAPI
+	productService *product.Service
+}
+
+func NewCommander(bot *tgbotapi.BotAPI, productService *product.Service) *Commander {
+	return &Commander{
+		bot: bot,
+	}
+}
---
+++ internal/app/commands/list.go
@@ -0,0 +1,17 @@
+package commands
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (c *Commander) List(inputMessage *tgbotapi.Message) {
+	outputMessageText := "Here all the products:\n\n"
+
+	products := c.productService.List()
+	for _, p := range products {
+		outputMessageText += p.Title + "\n"
+	}
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMessageText)
+	c.bot.Send(msg)
+}
